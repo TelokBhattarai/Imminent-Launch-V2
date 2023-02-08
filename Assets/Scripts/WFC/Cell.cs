e0c6a193d876e0733879a4040f6d74c841db56a3
Vishnu Kunnummal <vkunnummal1@gmail.com>
2023-02-08 12:44:49 -0500

    WFC files + scene

---
+++ Assets/Scripts/WFC/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Cell
+    {
+        // Key names for each tile in StandardPalette
+        const string blankKey = "blankKey";
+        const string upKey = "upKey";
+        const string rightKey = "rightKey";
+        const string downKey = "downKey";
+        const string leftKey = "leftKey";
+        const string failedKey = "failedKey";
+
+        public int x = -1;
+        public int y = -1;
+
+        public bool collapsed = false;
+
+        public List<string> options = new List<string>(){ blankKey, upKey, rightKey, downKey, leftKey };  // Total of five options - 5 tiles
+
+        public Cell() { }
+        public Cell(int x, int y) { this.x = x; this.y = y; }
+
+    }
---
+++ Assets/Scripts/WFC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WFC Generator");
+            
+            Generator gen = new Generator(50, 20);
+            gen.PeformWFC();
+        }
+    }
